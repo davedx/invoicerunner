236600539efe2999908825747a307a85030fedef
Dave Clayton <daclayton@libertyglobal.com>
2013-10-08 17:20:53 +0200

    Added mailchimp integration

--- model.js
+++ model.js
@@ -32,7 +32,25 @@ Invoices.allow({
 if (Meteor.isServer) {
 
   Meteor.methods({
-    upgradeAccount: function (options) {
+    addToMailList: function (options) {
+      var email = options.email;
+      var mckey = '841e25e309b9933d79c8c543b458c88e-us7';
+      var baseUrl = 'https://us7.api.mailchimp.com/2.0/';
+      var listId = '6ad8ee6eff';
+      var mcResult = HTTP.post(baseUrl + 'lists/subscribe', {data: {
+        apikey: mckey,
+        id: listId,
+        email: {
+          email: options.email
+        }
+      }});
+      if(mcResult.statusCode !== 200) {
+        console.error("Error connecting to MailChimp");
+        throw new Meteor.Error(500, 'Mailchimp error');
+      }
+    },
+
+     upgradeAccount: function (options) {
       
       var paymillKey = 'c807ea7cfc00c6faa443b629656a5834';
       
