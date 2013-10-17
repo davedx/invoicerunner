1929b361ca6adceeea905db6e9422e7a140f7238
Dave Clayton <davedx@gmail.com>
2013-10-17 22:50:13 +0200

    onLogin hook

---
+++ client/onLogin.js
@@ -0,0 +1,22 @@
+if(Meteor.isClient) {
+  Meteor.loginWithPassword = _.wrap(Meteor.loginWithPassword, function(login) {
+    // Store the original arguments
+    var args = _.toArray(arguments).slice(1),
+        user = args[0],
+        pass = args[1],
+        origCallback = args[2];
+
+    // Create a new callback function
+    // Could also be defined elsewhere outside of this wrapped function
+    var newCallback = function(err) {
+      if(!err) {
+        Router.go('invoices');
+      }
+    }
+
+    // Now call the original login function with
+    // the original user, pass plus the new callback
+    login(user, pass, newCallback);
+
+  });
+}
--- client/router.js
+++ client/router.js
@@ -67,4 +67,3 @@ Router.map(function () {
   });
 
 });
-
