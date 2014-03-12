b77d69a36a980968cbec74a12c97f016515a7062
Dave Clayton <davedx@gmail.com>
2014-03-12 15:35:07 +0100

    Layout - css refs broken

--- server/router.js
+++ server/router.js
@@ -4,13 +4,15 @@ Router.map(function () {
     where: 'server',
     path: '/',
     action: function() {
-      console.log("Returning hello");
+      //console.log("Returning hello");
       this.response.writeHead(200, {'Content-Type': 'text/html'});
       var public_freetrial = Handlebars.templates['public_freetrial'];
-      console.log(public_freetrial);
-      var html = Handlebars.templates['public_home']({},
+      //console.log(public_freetrial);
+      var public_home = Handlebars.templates['public_home']({},
       	{public_freetrial: public_freetrial});
-      this.response.end(html);
+      var public_html = Handlebars.templates['public_layout']({},
+      	{content: public_home});
+      this.response.end(public_html);
     }
   });
 
